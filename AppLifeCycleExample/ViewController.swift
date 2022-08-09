74e6261146c598e7b2f615c06e674fd7ddc2a618
Erika Bueno <erikab@ciandt.com>
2022-08-09 19:44:00 -0300

    Initial Commit

---
+++ AppLifeCycleExample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AppLifeCycleExample
+//
+//  Created by Erika C. Matesz Bueno on 09/08/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
